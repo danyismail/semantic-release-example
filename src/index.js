33113fd7edfa0a9754fa464f04ba4ea09d615ea2
dany <dany_arie@yahoo.com>
2025-05-16 17:15:54 +0700

    feat(user): add token 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 console.log('Halo dunia! Ini contoh proyek semantic-release.');
 console.log('Saya mencoba update versi.');
 console.log('Saya mencoba pake token');
+console.log('Saya mencoba pake token2');
